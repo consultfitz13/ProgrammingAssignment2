74c8bf1152529cc198199ee28207ff85980467da
consultfitz13 <consultfitz13@gmail.com>
2014-05-15 19:22:20 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,6 @@
-## Creates a cached copy of the inverse of a matrxi
+## Creates a cached copy of the inverse of a matrix
+## x - a square invertible matrix
+## returns a list that can function as a cached matrix
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -15,9 +17,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Return a matrix that is the inverse of 'x'
-## Note: Assumes that x is always invertible
-
+## Creates inverse of a matrix
+## Will return from the cache if previously assigned
+## x - a makeCacheMatrix object
+## Return a matrix that is the inverse of the matrix in 'x'
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
   if(!is.null(i)) {
